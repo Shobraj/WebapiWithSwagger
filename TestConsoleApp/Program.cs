9b640a032e357a8f2117d1e6897e3111174d1f8d
Shobraj <36963737+Shobraj@users.noreply.github.com>
2019-02-04 23:45:56 +0530

    edit program.cs

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -13,7 +13,8 @@ namespace TestConsoleApp
                 var todos = context.todos.ToList();
                 foreach(var todo in todos)
                 {
-                    Console.WriteLine(todo.TaskId, todo.TaskName);                    
+                    Console.WriteLine(todo.TaskId);
+                    Console.WriteLine(todo.TaskName);
                 }
                 Console.ReadLine();
             }
